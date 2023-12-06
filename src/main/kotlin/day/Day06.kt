79744f0780b4af6d8e6195936b28ef6fd42776aa
Maaike Wolters <maaike@drbreakalot.nl>
2023-12-06 15:34:40 +0100

    Solve day 6
    This could have been solved in trivial time with some calculus, but brute force solves it in 1.7 seconds :)

--- src/main/kotlin/day/Day06.kt
+++ src/main/kotlin/day/Day06.kt
@@ -1,19 +1,44 @@
 package day
 
+import asLongs
+
 object Day06 : Day {
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        return parse(input)
+            .map { race -> allDistancesInRace(race).filter { it > race.distance } }
+            .map { it.count() }
+            .fold(1) { acc, it -> acc * it }
+            .asSuccess()
     }
 
     override fun part2(input: List<String>): Result {
-        return NotImplemented
+        val nonWhitespace = "\\s+".toRegex()
+        return parse(input.map { nonWhitespace.replace(it, "") })
+            .map { race -> allDistancesInRace(race).filter { it > race.distance } }
+            .map { it.count() }
+            .first()
+            .asSuccess()
     }
+
+    fun parse(input: List<String>): List<Race> {
+        val times = input[0].substringAfter("Time:").trim().split("\\s+".toRegex()).asLongs()
+        val distances = input[1].substringAfter("Distance:").trim().split("\\s+".toRegex()).asLongs()
+        return times.zip(distances) { time, distance -> Race(time, distance) }
+    }
+
+    fun distanceTravelledInTime(totalTime: Long, buttonPressTime: Long): Long =
+        (totalTime - buttonPressTime) * buttonPressTime
+
+    fun allDistancesInRace(race: Race): List<Long> = (1..<race.time).map { distanceTravelledInTime(race.time, it) }
+
+    data class Race(val time: Long, val distance: Long)
 
     override fun testData(): Day.TestData {
         return Day.TestData(
-            0,
-            0,
-            """"""
+            288,
+            71503,
+            """Time:      7  15   30
+Distance:  9  40  200"""
                 .lines()
         )
     }
