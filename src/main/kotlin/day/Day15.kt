7f5e693f70e1cfbce13bc6fa8c628329f85ea65e
Maaike Wolters <maaike@drbreakalot.nl>
2023-12-15 13:26:01 +0100

    Solve day 15 part 1

--- src/main/kotlin/day/Day15.kt
+++ src/main/kotlin/day/Day15.kt
@@ -2,8 +2,15 @@ package day
 
 object Day15 : Day {
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        return input.first()
+            .split(",")
+            .sumOf { hash(it) }
+            .asSuccess()
     }
+
+    fun hash(input: String): Int = input.map { it.code }
+        .fold(0) { acc, it -> ((acc + it) * 17) % 256 }
+
 
     override fun part2(input: List<String>): Result {
         return NotImplemented
@@ -11,9 +18,9 @@ object Day15 : Day {
 
     override fun testData(): Day.TestData {
         return Day.TestData(
+            1320,
             0,
-            0,
-            """"""
+            """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
                 .lines()
         )
     }
