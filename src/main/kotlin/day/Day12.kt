0887c8650a1edbb4aedd46235a98b2170d889f0e
Maaike Wolters <maaike@drbreakalot.nl>
2023-12-16 21:13:05 +0100

    Solve day 12 part 2 (finally!)
    Solves about 10x as fast as part 1 now that I've actually got a recursive memoized solution
    In other news: the syntax when trying recursive memoization in kotlin gets really ugly really quick

--- src/main/kotlin/day/Day12.kt
+++ src/main/kotlin/day/Day12.kt
@@ -1,6 +1,9 @@
 package day
 
+import Memo2
 import asInts
+import memoize
+import repeat
 import split
 
 object Day12 : Day {
@@ -23,7 +26,24 @@ object Day12 : Day {
     }
 
     override fun part2(input: List<String>): Result {
-        return NotImplemented
+        val validArrangementCounts = input.map { parse(it) }
+            .map { (springs, record) -> repeatSpringWithUnknownInBetween(springs) to record.repeat(5) }
+            .map { (springs, record) -> part2SolveMemoized(springs, record) }
+        return validArrangementCounts.sum().asSuccess()
+    }
+
+    fun repeatSpringWithUnknownInBetween(springs: List<SpringCondition>): List<SpringCondition> {
+        val ret = mutableListOf<SpringCondition>()
+        ret.addAll(springs)
+        ret.add(SpringCondition.Unknown)
+        ret.addAll(springs)
+        ret.add(SpringCondition.Unknown)
+        ret.addAll(springs)
+        ret.add(SpringCondition.Unknown)
+        ret.addAll(springs)
+        ret.add(SpringCondition.Unknown)
+        ret.addAll(springs)
+        return ret
     }
 
     fun generatePossibleConditions(from: List<SpringCondition>): Sequence<List<SpringCondition>> {
@@ -51,6 +71,8 @@ object Day12 : Day {
         val workingSpringSizes = springs.split(SpringCondition.Broken).map { it.size }.filter { it != 0 }
         return workingSpringSizes == record
     }
+
+    val part2SolveMemoized = Memo2<List<SpringCondition>, List<Int>, Long>::calculateValidConditionsCount.memoize()
 
     enum class SpringCondition {
         Unknown,
@@ -80,4 +102,45 @@ object Day12 : Day {
                 .lines()
         )
     }
+}
+
+private fun Memo2<List<Day12.SpringCondition>, List<Int>, Long>.calculateValidConditionsCount(springs: List<Day12.SpringCondition>, record: List<Int>): Long {
+    if (springs.isEmpty() && record.isNotEmpty()) {
+        return 0L
+    }
+    if (springs.isEmpty() && record.isEmpty()) {
+        return 1L
+    }
+
+    return when (springs.first()) {
+        Day12.SpringCondition.Unknown -> calculateValidWorkingConditionsCount(springs, record) +
+                recurse(springs.subList(1, springs.size), record)
+
+        Day12.SpringCondition.Working -> calculateValidWorkingConditionsCount(springs, record)
+        Day12.SpringCondition.Broken -> recurse(springs.subList(1, springs.size), record)
+    }
+}
+
+private fun calculateValidWorkingConditionsCount(springs: List<Day12.SpringCondition>, record: List<Int>): Long {
+    if (record.isEmpty()) {
+        return 0L
+    }
+    val expectedWorkingCount = record.first()
+    if (springs.size < expectedWorkingCount) {
+        return 0L
+    }
+    if (springs.subList(0, expectedWorkingCount).all { it != Day12.SpringCondition.Broken }) {
+        // if end of springs and end of record return 1
+        return if (springs.size == expectedWorkingCount) {
+            if (record.size == 1) 1L else 0L
+        } else if (springs[expectedWorkingCount] != Day12.SpringCondition.Working) {
+            Day12.part2SolveMemoized(
+                springs.subList(expectedWorkingCount + 1, springs.size),
+                record.subList(1, record.size)
+            )
+        } else {
+            0L
+        }
+    }
+    return 0L
 }
