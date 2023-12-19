c980d9edc5490531446a7f8120eeb6e9a5e63916
Maaike Wolters <maaike@drbreakalot.nl>
2023-12-19 03:04:50 +0100

    Solve day 18 part 2

--- src/main/kotlin/day/Day18.kt
+++ src/main/kotlin/day/Day18.kt
@@ -6,7 +6,6 @@ import ResizingMutableRectangle
 import Side
 import neighbour
 import neighbours
-import toString
 
 object Day18 : Day {
     override fun part1(input: List<String>): Result {
@@ -35,8 +34,29 @@ object Day18 : Day {
         return digMap.count { it }.asSuccess()
     }
 
+    override fun part2(input: List<String>): Result {
+        val instructions = input.map { parse2(it) }
+        var insideArea = 0L
+        var coordinate = Coordinate(0,0)
+        instructions.forEach {
+            val newCoordinate = when (it.direction) {
+                Side.North -> coordinate.copy(y = coordinate.y - it.count)
+                Side.East -> coordinate.copy(x = coordinate.x + it.count)
+                Side.South -> coordinate.copy(y = coordinate.y + it.count)
+                Side.West -> coordinate.copy(x = coordinate.x - it.count)
+            }
+            insideArea += (coordinate.y + newCoordinate.y) * (coordinate.x - newCoordinate.x)
+            coordinate = newCoordinate
+        }
+        insideArea /= 2
+        val borderArea = instructions.sumOf { it.count }
+        val picks = insideArea + borderArea / 2 + 1
+
+        return picks.asSuccess()
+    }
+
     fun parse(line: String): Instruction {
-        val (directionText, countText, colorText) = line.split(" ")
+        val (directionText, countText, _) = line.split(" ")
         val direction = when (directionText) {
             "U" -> Side.North
             "R" -> Side.East
@@ -45,8 +65,21 @@ object Day18 : Day {
             else -> throw IllegalArgumentException("Unknown direction: $directionText")
         }
         val count = countText.toInt()
-        val color = colorText.removeSurrounding("(#", ")").toInt(16)
-        return Instruction(direction, count, color)
+        return Instruction(direction, count)
+    }
+
+    fun parse2(line: String): Instruction {
+        val (_, _, colorText) = line.split(" ")
+        val color = colorText.removeSurrounding("(#", ")")
+        val count = color.take(5).toInt(16)
+        val direction = when (color.last()) {
+            '0' -> Side.East
+            '1' -> Side.South
+            '2' -> Side.West
+            '3' -> Side.North
+            else -> throw IllegalArgumentException("Unknown direction: $color")
+        }
+        return Instruction(direction, count)
     }
 
     private fun floodFill(digMap: MutableRectangle<Boolean>, x: Int, y: Int) {
@@ -56,17 +89,13 @@ object Day18 : Day {
         }
     }
 
-    data class Instruction(val direction: Side, val count: Int, val color: Int)
-
-    override fun part2(input: List<String>): Result {
-        //I'm not getting paid enough for this shit
-        return NotImplemented
-    }
+    data class Instruction(val direction: Side, val count: Int)
+    data class Coordinate(val x: Long, val y: Long)
 
     override fun testData(): Day.TestData {
         return Day.TestData(
-            62,
-            0,
+            "62",
+            "952408144115",
             """R 6 (#70c710)
 D 5 (#0dc571)
 L 2 (#5713f0)
